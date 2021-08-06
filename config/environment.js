511817989c5f860011c810d390e66feec4e46b13
Aidan Bennett <aidanbennett1103@gmail.com>
2021-08-06 14:21:51 +0100

    Hide github for now

--- config/environment.js
+++ config/environment.js
@@ -38,10 +38,10 @@ module.exports = function (environment) {
           route: "page",
           id: "aidan-bennett",
         },
-        {
-          label: "Github",
-          route: "link",
-          id: "https://github.com/aidanbennett2",
+     //   {
+      //    label: "Github",
+       //   route: "link",
+       //   id: "https://github.com/aidanbennett2",
         },
       ],
     },
